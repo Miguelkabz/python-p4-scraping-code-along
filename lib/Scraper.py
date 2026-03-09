30808378dac264680ab2028a76b7ea5dc7968fec
Miguelkabz <kabzmiguel@gmail.com>
2026-03-09 23:34:46 +0300

    done

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -5,4 +5,25 @@ import ipdb
 
 
 class Scraper:
-    pass
+    def __init__(self):
+        self.url = "http://learn-co-curriculum.github.io/site-for-scraping/courses"
+
+    def get_page(self):
+        response = requests.get(self.url)
+        return BeautifulSoup(response.content, 'html.parser')
+
+    def get_courses(self):
+        doc = self.get_page()
+        articles = doc.find_all('article', class_='post')
+        return [a for a in articles if a.find('h2')]
+
+    def make_courses(self):
+        courses = []
+        course_elements = self.get_courses()
+        for element in course_elements:
+            title = element.find('h2').text.strip()
+            schedule = element.find('em', class_='date').text.strip()
+            description = element.find('p').text.strip()
+            course = Course(title=title, schedule=schedule, description=description)
+            courses.append(course)
+        return courses
